825dbc49c425dc62ae6a7225822bbf9d9df3f121
一戸悠河 <yuga@ichinoheyuuga-no-MacBook-Pro.local>
2016-11-30 06:52:42 +0800

    functions.phpの追加

---
+++ functions.php
@@ -0,0 +1,18 @@
+<?php
+    define('DEBUG', true);
+
+    function special_echo($val) {
+        if (DEBUG) {
+            echo $val;
+            echo '<br>';
+        }
+    }
+
+    function special_var_dump($val) {
+        if (DEBUG) {
+            echo '<pre>';
+            var_dump($val);
+            echo '</pre>';
+        }
+    }
+ ?>
