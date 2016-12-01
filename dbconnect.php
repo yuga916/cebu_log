3ef7fe1328c8e5be89160beb8f4dd892422521a5
yuga <yuga.ichinohe@gmail.com>
2016-12-01 09:33:53 +0800

    dbconnect.phpの修正

--- dbconnect.php
+++ dbconnect.php
@@ -1,7 +1,7 @@
 <?php
     // dbconnect.php
 
-    $db = mysqli_connect('localhost', 'root', 'mysql', 'seed_blog')
+    $db = mysqli_connect('localhost', 'root', 'mysql', 'Cebu_blog')
       or die(mysqli_connect_error());
 
     mysqli_set_charset($db, 'utf8');
