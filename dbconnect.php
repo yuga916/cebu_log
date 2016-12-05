707e99a1700f9ba21eb2d3177c6859f62a4dfc92
DESKTOP-810S26R\jdaif <dai.kanto20@gmail.com>
2016-12-05 09:49:31 +0800

    UPLOAD用のフォルダ作成

--- dbconnect.php
+++
@@ -1,8 +0,0 @@
-<?php
-    // dbconnect.php
-
-    $db = mysqli_connect('localhost', 'root', 'mysql', 'cebu_blog')
-      or die(mysqli_connect_error());
-
-    mysqli_set_charset($db, 'utf8');
- ?>
