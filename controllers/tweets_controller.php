3652543527a4a5e46e27eef092e58317d1337879
石井篤美 <ishiiatsumi@ishiiatsumi-no-MacBook-Air.local>
2016-12-06 11:54:25 +0800

    initial commit

---
+++ controllers/tweets_controller.php
@@ -0,0 +1,9 @@
+<?php 
+    // Controllerのクラス
+      // tweet：つぶやきフォーム
+      // tweet：プルダウン内容選択「food・shop」
+      // tweet：つぶやき降順（新→古）表示
+      // tweet：返信
+      // tweet：投稿削除（投稿者のみ）
+      // tweet：いいね
+ ?>
