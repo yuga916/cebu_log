8844649e24f13c0d0010c4853311ac2f492d73e2
DESKTOP-810S26R\jdaif <dai.kanto20@gmail.com>
2016-12-06 11:03:41 +0800

    お店の新規投稿、登録完了 エラー表示が未実装

--- models/shop.php
+++ models/shop.php
@@ -2,7 +2,7 @@
   special_echo('Modelのpictures.phpが呼び出されました。');
 
  //model class 
-  class Picture{
+  class Shop{
   	//プロパティ
   	private $dbconnect;
 
@@ -12,49 +12,34 @@
   	$this->dbconnect=$db;
   	}
 
-    function post_validation($post,$files){
+    function post_validation($post){
         $error=array();
             //owner_idの未入力チェック
             if($post['owner_id']==''){
               $error['owner_id']='blank';
             }
             //お店の名前のID未入力チェック
-            if($post['s_id']==''){
-                $error['s_id']='blank';
+            if($post['shop_name']==''){
+                $error['shop_name']='blank';
             }
              //categolyの未入力チェック
-            if($post['categoly']==''){
-                $error['categoly']='blank';
+            if($post['intro_shop']==''){
+                $error['intro_shop']='blank';
             }
-            if ($files['picture_path']==''){
-                $error['picture_path']='blank';
+            if ($post['address']==''){
+                $error['address']='blank';
             }
                                      
      return $error;
     }
 
-    function create($post,$files){
+    function create($post){
       special_echo('modelsのcreateが呼び出された' );
-      $sql=sprintf('INSERT INTO `pictures` SET `owner_id`=%d,`categoly`=%d,`s_id`=%d,`shop_picture_path`="%s",`created`=NOW()',mysqli_real_escape_string($this->dbconnect,$post['owner_id']),mysqli_real_escape_string($this->dbconnect,$post['categoly']),mysqli_real_escape_string($this->dbconnect,$post['s_id']),mysqli_real_escape_string($this->dbconnect,$files)
-        );
-      //mysqli_query($this->dbconnect,$sql) or die(mysqli_error($this->dbconnect));
+      $sql=sprintf('INSERT INTO `shops` SET `shop_name`="%s",`shop_intro`="%s",`owner_id`=%d,`shop_address`="%s",`created`=NOW()',mysqli_real_escape_string($this->dbconnect,$post['shop_name']),mysqli_real_escape_string($this->dbconnect,$post['intro_shop']),mysqli_real_escape_string($this->dbconnect,$post['owner_id']),mysqli_real_escape_string($this->dbconnect,$post['address']));
+      mysqli_query($this->dbconnect,$sql) or die(mysqli_error($this->dbconnect));
 
     }
 
-    function add_shops(){
-      special_echo('modelのadd_shopsが呼び出された');
-      //shopテーブルからshop_idとshop_nameと取り出す
-      $sql='SELECT`shop_id`,`shop_name`FROM `shops`';
-      $results=mysqli_query($this->dbconnect,$sql) or die(mysqli_error($this->dbconnect));
-      return $results;
-    }
 
-    function add_categoly(){
-      special_echo('modelのadd_categolyが呼び出された');
-      //categolyテーブルからcategoly_idとcategolyを取り出す
-      $sql='SELECT*FROM `categoly`';
-      $results=mysqli_query($this->dbconnect,$sql) or die(mysqli_error($this->dbconnect));
-      return $results;
-    }
   }
 ?>
