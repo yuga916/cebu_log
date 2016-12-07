d3206a07dc44253495abeb87bc7234efce9fc5d6
石井篤美 <ishiiatsumi@ishiiatsumi-no-MacBook-Air.local>
2016-12-07 11:46:45 +0800

    チェック待ち（チェックをもって完了）

--- views/tweets/index.php
+++ views/tweets/index.php
@@ -1,5 +1,14 @@
-<?php 
-      // 新規tweet本文
+<!--  tweet：つぶやきフォーム -->
+
+<!-- 新規投稿ボタン -->
+<p><a href="/cebu_log/tweets/create" class="btn btn-info">新規投稿</a></p>
+    <?php foreach($this->viewOptions as $viewOption) ?>
+<!-- 新規tweet本文 -->
+    <div class="msg">
+          <p><a href="show/<?php echo $viewOption['id']; ?>"><?php echo $viewOption['title']; ?></a></p>
+          <p class="day"><?php echo $viewOption['created']; ?></p>
+        </div>
+      <?php endforeach; ?>
+<!--  // tweet：プルダウン内容選択「food・shop」
       // 編集
-      // 削除
- ?>
+      // 削除 -->
