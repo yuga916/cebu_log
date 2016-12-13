beb921ab9a8e53a4d59564f4ece78b5bd44a431e
石井篤美 <ishiiatsumi@ishiiatsumi-no-MacBook-Air.local>
2016-12-13 09:26:03 +0800

    枠組み作成

--- views/tweets/index.php
+++ views/tweets/index.php
@@ -1,14 +1,43 @@
-<!--  tweet：つぶやきフォーム -->
-
+<!--  Tweet：つぶやきフォーム -->
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+  </head>
+<body>
+<!-- 新規投稿入力 -->
+      <div class="msg">
+        <form method="post" action="" class="form-horizontal" role="form">
+            <div class="col-md-9">
+              <textarea name="body" class="form-control" cols="30" rows="5"></textarea>
+            </div>
+          <div class="form-group pull-right">
+<!--  Tweet：プルダウン内容選択「food・shop」-->
+              <select>
+                <option value="food" id="food">food</option>
+                <option value="shop" id="shop">shop</option>
+                <option value="other" id="other">other</option>
+              </select>
 <!-- 新規投稿ボタン -->
-<p><a href="/cebu_log/tweets/create" class="btn btn-info">新規投稿</a></p>
-    <?php foreach($this->viewOptions as $viewOption) ?>
-<!-- 新規tweet本文 -->
+              <p><a href="/cebu_log/tweets/create" class="btn btn-info">新規投稿</a></p>
+              <?php foreach($this->viewOptions as $viewOption): ?><br><br>
+          </div>
+        </form>
+      </div>
+
+<!-- 降順Tweet本文 -->
     <div class="msg">
-          <p><a href="show/<?php echo $viewOption['id']; ?>"><?php echo $viewOption['title']; ?></a></p>
+          <p><a href="show/<?php echo $viewOption['tweet_id']; ?>">
+          <?php echo $viewOption['tweet']; ?></a></p>
           <p class="day"><?php echo $viewOption['created']; ?></p>
+<!-- 編集 削除 -->
+          [<a href="edit/<?php echo $viewOption['id']; ?>" style="color: #00994C;">返信</a>]
+          [<a href="delete/<?php echo $viewOption['id']; ?>" style="color: #F33;">削除</a>]
         </div>
       <?php endforeach; ?>
-<!--  // tweet：プルダウン内容選択「food・shop」
-      // 編集
-      // 削除 -->
+</body>
+</html>
+
