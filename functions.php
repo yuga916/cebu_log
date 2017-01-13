27d2bc01f21353f18a334683b0b836241ff500a7
dai <dai.kanto20@gmail.com>
2017-01-13 12:55:26 +0900

    修正

--- functions.php
+++ functions.php
@@ -17,5 +17,22 @@
     }
 
    
+    function makePath() {
+         if (DEBUG) {
+           return '/cebu_log/';
+         } else {
+           return '/portfolio/cebu_log/';
+         }
+    }
+
+    function own_header($path) {
+        if (DEBUG) {
+          return '<script>window.location.href = "/cebu_log/' . $path . '";</script>';
+        } else {
+          return '<script>window.location.href = "/portfolio/cebu_log/' . $path . '";</script>';
+        }
+
+    }
+
 
  ?>
