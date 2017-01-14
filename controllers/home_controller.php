30ac90e05568fd2974cc50f7723e62ad376c2a51
dai <dai.kanto20@gmail.com>
2017-01-14 15:41:42 +0900

    修正

--- controllers/home_controller.php
+++ controllers/home_controller.php
@@ -59,6 +59,7 @@
             special_var_dump($this->viewOptions);
             $this->viewlikesranks=$this->user->shopslikesranking();
             special_var_dump($this->viewlikesranks);
+            special_var_dump($this->viewlikesranks);
             $this->viewrealtimepics=$this->picture->realtime_top();
             special_var_dump($this->viewlikesranks);
             special_var_dump($this->viewrealtimepics);
